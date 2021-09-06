d58fbb1332144a1f2fceba832794d934dd936fb5
Leandro Lesnik <52947547+leandrolesnik@users.noreply.github.com>
2021-09-06 15:44:33 -0300

    Adicionado a ligação com o firebase

---
+++ web/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAw6r-cmFowhTr1iZ7AcofMbEogAX-gJ7c",
+  authDomain: "bootiquim.firebaseapp.com",
+  projectId: "bootiquim",
+  storageBucket: "bootiquim.appspot.com",
+  messagingSenderId: "151109585446",
+  appId: "1:151109585446:web:85d9325ea00aea1ba8f4e7"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
