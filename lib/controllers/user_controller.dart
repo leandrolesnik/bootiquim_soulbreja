3c03b7f570689d10954521d3fe5cebfb13371b0b
Leandro Lesnik <52947547+leandrolesnik@users.noreply.github.com>
2021-09-06 17:09:33 -0300

    criado as páginas de login e cadastro

--- lib/controllers/user_controller.dart
+++ lib/controllers/user_controller.dart
@@ -1,1 +1,58 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import '../models/user_model.dart';
+
+enum AuthState { signed, unsigned, loading }
+
+class UserController extends ChangeNotifier {
+  AuthState authState = AuthState.loading;
+  late UserModel model;
+
+  final _auth = FirebaseAuth.instance;
+  final _db = FirebaseFirestore.instance;
+
+  User? get user => _auth.currentUser;
+
+  UserController() {
+    _auth.authStateChanges().listen((user) async {
+      if (user != null) {
+        authState = AuthState.signed;
+        final data = await _db.collection('usuarios').doc(user.uid).get();
+        model = UserModel.fromMap(data.data()!);
+      } else {
+        authState = AuthState.unsigned;
+      }
+      notifyListeners();
+    });
+  }
+
+  Future<void> login(String email, String senha) async {
+    await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: senha,
+    );
+  }
+
+  Future<void> logout() async {
+    await _auth.signOut();
+  }
+
+  Future<void> signup(
+    String email,
+    String senha,
+    UserModel payload,
+  ) async {
+    final credentials = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: senha,
+    );
+
+    final uid = credentials.user?.uid;
+    final data = payload.toMap();
+    data['id'] = uid;
+
+    final doc = _db.collection('usuarios').doc(uid);
+    await doc.set(data);
+  }
+}
